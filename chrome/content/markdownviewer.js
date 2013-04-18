af7e5cdc62400e11d2b239cb0931239899cfa020
thiht <thiht44@gmail.com>
2013-04-18 10:34:26 +0200

    First commit

---
+++ chrome/content/markdownviewer.js
@@ -0,0 +1,39 @@
+window.addEventListener('load', function load(event) {
+	window.removeEventListener('load', load, false);
+	markdownviewer.init();
+}, false);
+
+if (typeof markdownviewer === 'undefined') {
+	var markdownviewer = {
+
+		init: function() {
+			var appcontent = document.getElementById('appcontent');
+			if (appcontent) {
+				appcontent.addEventListener('DOMContentLoaded', this.onPageLoad, true);
+			}
+		},
+
+		onPageLoad: function(aEvent) {
+			var document = aEvent.originalTarget,
+			    regexpMdFile = /\.m(arkdown|kdn?|do?(wn)?)$/i;
+
+			if (regexpMdFile.test(document.location)) {
+				marked.setOptions({
+					gfm: true,
+					pedantic: false,
+					sanitize: false
+				});
+
+				var content = document.firstChild;
+				content.innerHTML = '<!DOCTYPE html>' +
+				                    '<head>' +
+				                    '    <title>Markdown Viewer</title>' +
+				                    '    <link rel="stylesheet" type="text/css" href="resource://mdskin/bootstrapLite.css">' +
+				                    '</head>' +
+				                    '<body class="container">' +
+				                        marked(content.textContent) +
+				                    '</body>';
+			}
+		}
+	};
+}
