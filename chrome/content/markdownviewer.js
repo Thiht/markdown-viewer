94e2b30448babc88e3213aa55aa590330fdae69c
wonder365 <wonder365@gmail.com>
2013-04-26 14:59:34 +0800

    add syntax highlight for code

--- chrome/content/markdownviewer.js
+++ chrome/content/markdownviewer.js
@@ -21,7 +21,8 @@ if (typeof markdownviewer === 'undefined') {
 				marked.setOptions({
 					gfm: true,
 					pedantic: false,
-					sanitize: false
+					sanitize: false,
+					langPrefix: 'language-'
 				});
 
 				var content = document.firstChild;
@@ -29,10 +30,20 @@ if (typeof markdownviewer === 'undefined') {
 				                    '<head>' +
 				                    '    <title>Markdown Viewer</title>' +
 				                    '    <link rel="stylesheet" type="text/css" href="resource://mdskin/bootstrapLite.css">' +
+						    '    <link rel="stylesheet" type="text/css" href="resource://mdskin/default.min.css">' +
 				                    '</head>' +
 				                    '<body class="container">' +
 				                        marked(content.textContent) +
 				                    '</body>';
+				var loadjsfile=function(doc, jsfile){
+					var script = doc.createElement("script");
+					script.type = "text/javascript";
+					script.src = "resource://mdskin/"+jsfile;
+					var head = doc.getElementsByTagName("head")[0] || doc.documentElement;
+					head.insertBefore(script, head.firstChild);
+				};
+				loadjsfile(document, 'highlight.min.js');
+				loadjsfile(document, 'inithighlight.js'); 
 			}
 		}
 	};
