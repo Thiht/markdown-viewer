Thiht/markdown-viewer

commit 25d124bc6ec471b964be401d6013ff803d8e2ddd

Fix regexp to detect markdown file (URL with GET parameters)

chrome/content/markdown-viewer.js
@@ -16,7 +16,7 @@ if (!MarkdownViewer) {
 
 		onPageLoad: function(aEvent) {
 			var document = aEvent.originalTarget;
-			var markdownFileExtension = /\.m(arkdown|kdn?|d(o?wn)?)(#.*)?$/i;
+			var markdownFileExtension = /\.m(arkdown|kdn?|d(o?wn)?)(#.*)?(.*)$/i;
 
 			if (document.location.protocol !== "view-source:"
 				&& markdownFileExtension.test(document.location.href)) {

commit 97550e46626094709fbc55014be95a94eea66552

Strictly match URLs with parameters
Previously the trailing .* erroneously matched URLs like
https://iamnotmarkdown.mdnotmarkdown.bug.  Regression from
25d124bc6ec471b964be401d6013ff803d8e2ddd.  Fixes #51.

chrome/content/markdown-viewer.js
@@ -64,7 +64,7 @@ if (!MarkdownViewer) {
 
 		onPageLoad: function(aEvent) {
 			const document = aEvent.originalTarget;
-			const markdownFileExtension = /\.m(arkdown|kdn?|d(o?wn)?)(#.*)?(.*)$/i;
+			const markdownFileExtension = /\.m(arkdown|kdn?|d(o?wn)?)(\?.*)?(#.*)?$/i;
 
 			if (document.location.protocol !== "view-source:" &&
 				markdownFileExtension.test(document.location.href) &&