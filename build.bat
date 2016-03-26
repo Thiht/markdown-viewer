eec8d961a03300c35f643a80df7f224d7cd586fa
Thibaut Rousseau <thibaut.rousseau@protonmail.com>
2016-03-26 15:11:56 +0100

    Improve build system

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
-7z a -tzip -scsUTF-8 markdown-viewer@thiht.fr.xpi .\chrome.manifest .\install.rdf .\LICENSE .\README.md chrome components
+del .\markdown-viewer@thiht.fr.xpi
+7z a -tzip -scsUTF-8 markdown-viewer@thiht.fr.xpi .\chrome.manifest .\install.rdf .\LICENSE .\README.md .\components .\chrome\content .\chrome\skin .\chrome\lib\highlightjs\highlight.pack.min.js .\chrome\lib\markdown-it\dist\markdown-it.min.js
 pause
--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-zip -r markdown-viewer@thiht.fr.xpi chrome.manifest install.rdf LICENSE README.md chrome components
+rm markdown-viewer@thiht.fr.xpi 2> /dev/null
+zip -r markdown-viewer@thiht.fr.xpi chrome.manifest install.rdf LICENSE README.md components \
+  chrome/content chrome/skin chrome/lib/highlightjs/highlight.pack.min.js \
+  chrome/lib/markdown-it/dist/markdown-it.min.js
